af155507a5cb1a24385837d417cf002440dd85b8
AhmedEsaam <ahmed.e.mogy@gmail.com>
2024-07-06 16:18:31 +0300

    update

--- Bash Script/lab4.sh
+++ Bash Script/lab4.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-options=("display date and time" "List files in the current directory" "Display current disk usage" "Exit")
 
+# options array
+options=(
+    "display date and time"
+    "List files in the current directory"
+    "Display current disk usage"
+    "Exit")
+
+# changing the PS3 variable
 PS3="Please enter your choice: "
 
+# Displaying options and asking user to select one choice
 select option in "${options[@]}"
 do
     case $option in
