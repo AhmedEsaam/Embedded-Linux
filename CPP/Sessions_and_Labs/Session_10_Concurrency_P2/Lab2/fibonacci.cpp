4513644b7277a765e179460a52daf6079291b22c
AhmedEsaam <ahmed.e.mogy@gmail.com>
2024-07-31 18:54:23 +0300

    Adding fibonacci series lab

--- CPP/Sessions_and_Labs/Session_10_Concurrency_P2/Lab2/fibonacci.cpp
+++ CPP/Sessions_and_Labs/Session_10_Concurrency_P2/Lab2/fibonacci.cpp
@@ -19,6 +19,8 @@ void fibonacci_recursive(int count, int &result, int current = 1, int prev = 0)
     result = current + prev;
     prev = current;
     current = result;
+
+    // std::this_thread::sleep_for(std::chrono::milliseconds(10));
     fibonacci_recursive(count - 1, result, current, prev);
 }
 
@@ -36,12 +38,18 @@ void print_fibonacci(int count, int &result)
 
 int main()
 {
-    int result_1;
-    std::jthread fib_thread([result_1](int count)
+    int result = 0, count;
+
+    std::cout << "Please enter number: ";
+    std::cin >> count;
+
+    std::jthread fib_thread([&result, count]()
                             {
                             std::lock_guard<std::mutex> lock(fib_mutex);
-                            fibonacci_recursive(count, result_1, );
-                            fact_cv.notify_all(); }, 20);
+                            fibonacci_recursive(count, std::ref(result));
+                            results_ready.notify_all(); });
+
+    std::jthread thread_print(print_fibonacci, count, std::ref(result));
 
     return 0;
 }
